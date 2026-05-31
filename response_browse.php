88b025c0118b10dbd65dbfc099fbc8a43a93c5fe
Sebastian Thörn <b23sebth@student.his.se>
2026-05-31 21:11:49 +0200

    Added styling and return to homepage for repos
    Signed-off-by: Sebastian Thörn <b23sebth@student.his.se>

--- response_browse.php
+++ response_browse.php
@@ -1,13 +1,19 @@
 <html>
 
-  <?php 
-    if (isset($_POST['repo'])) {
-    $repo = $_POST['repo']; 
-    echo "<h2>" . $repo . "</h2>";
-    } else {
-      header("location: index.php");
-    }
-  ?>
+  <head>
+    <title>Github Service</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+    
+    <nav>
+      <a id='return-homepage' href='index.php'>Homepage</a>
+    </nav>
+
 
 </html>
 
