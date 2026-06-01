7ad994606bb423fe652ede49fb97a71a04d5f386
Sebastian Thörn <b23sebth@student.his.se>
2026-06-01 15:58:18 +0200

    Simple styling for table elements
    Signed-off-by: Sebastian Thörn <b23sebth@student.his.se>

--- response_browse.php
+++ response_browse.php
@@ -45,27 +45,28 @@
       echo "<div id='browse-repos'>";
 
         echo "<h1>Now displaying: <a href='" . $choosenRepo->getAttribute('url') . " '>" . $repo . "</a> </h1>";
-          echo "<table>";
-          echo "<caption>Files in repo (unsorted)</caption>";
-          foreach($singleFiles as $node) {
-            echo "<tr>";
-              echo "<td>";
-                echo "<table>";
-                  echo "<thead>";
-                    echo "<tr><th>Full name</th><th>Name</th><th>URL</th></tr>";
-                  echo "</thead>";
-                  echo "<tbody>";
-                    echo "<tr>";
-                      echo "<td>" . $node->getAttribute('fullname') . "</td>";
-                      echo "<td>" . $node->getAttribute('name') . "</td>";
-                      echo "<td> <a href='" . $node->getAttribute('url') . "'>" . $node->getAttribute('url') . "</a></td>";
-                    echo "</tr>";
-                  echo "</tbody>";
-                echo "</table>";
-              echo "</td>";
-            echo "</tr>";
-          }
-          echo "</table>";
+        # Using a row-layout.
+        echo "<table>";
+        echo "<caption>Files in repo (unsorted)</caption>";
+        foreach($singleFiles as $node) {
+          echo "<tr>";
+            echo "<td>";
+              echo "<table>";
+                echo "<thead>";
+                  echo "<tr><th>Full name</th><th>Name</th><th>URL</th></tr>";
+                echo "</thead>";
+                echo "<tbody>";
+                  echo "<tr>";
+                    echo "<td>" . $node->getAttribute('fullname') . "</td>";
+                    echo "<td>" . $node->getAttribute('name') . "</td>";
+                    echo "<td> <a href='" . $node->getAttribute('url') . "'>" . $node->getAttribute('url') . "</a></td>";
+                  echo "</tr>";
+                echo "</tbody>";
+              echo "</table>";
+            echo "</td>";
+          echo "</tr>";
+        }
+        echo "</table>";
 
       echo "</div>";
     ?>
