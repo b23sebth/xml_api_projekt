a51c54f108f453c470ecdbdc3315be917847a89f
Sebastian Thörn <b23sebth@student.his.se>
2026-06-01 17:11:03 +0200

    Initialized response_author
    Signed-off-by: Sebastian Thörn <b23sebth@student.his.se>

--- response_author.php
+++ response_author.php
@@ -1,1 +1,34 @@
 <!doctype html>
+<html>
+  <head>
+    <?php
+      if (!isset($_GET['login'])) {
+        header("location: index.php");
+      } else {
+        $author = $_GET['login']; 
+        echo "<title>" . $author . "</title>";
+      }
+    ?>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+    
+    <nav>
+      <a id='return-homepage' href='index.php'>Homepage</a>
+    </nav>
+
+    <?php
+      $xml = file_get_contents('https://wwwlab.webug.se/examples/XML/githubservice/commits/?author=' . $author);
+      $dom = new DomDocument;
+      $dom->preserveWhiteSpace = FALSE;
+      $dom->loadXML($xml);
+
+    ?>
+  
+  </body>
+
+</html>
