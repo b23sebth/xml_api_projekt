0191225c6e40aecf2e6158bd6941bf6baa5ec586
Sebastian Thörn <b23sebth@student.his.se>
2026-06-01 16:05:52 +0200

    Initialized response_search
    Signed-off-by: Sebastian Thörn <b23sebth@student.his.se>

--- response_search.php
+++ response_search.php
@@ -1,14 +1,27 @@
 <!doctype html>
 <html>
 
-  <?php 
-    if (isset($_POST['search'])) {
-    $search = $_POST['search']; 
-    echo "<h2>" . $search . "</h2>";
-    } else {
+  <head>
+    <?php 
+      if (!isset($_POST['search'])) {
       header("Refresh:0; url=index.php");
-    }
-  ?>
+      } else {
+        $search = $_POST['search']; 
+        echo "<title>" . $search . "</title>";
+      }
+    ?>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  </head>
+  
+  <body>
+    <nav>
+      <a id='return-homepage' href='index.php'>Homepage</a>
+    </nav>
+
+  </body>
 
 </html>
 
